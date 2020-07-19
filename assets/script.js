454bec5781192fd7ca8e9479e1d2c26f8848f2b5
Hachem Said <labsar.ahmed@outlook.fr>
2020-07-19 17:04:56 -0400

    fixed forecast bug

--- assets/script.js
+++ assets/script.js
@@ -181,6 +181,7 @@ $(document).ready(function () {
         var foreTemp = (((listEl.main.temp - 273.15) * 9) / 5 + 32).toFixed(1);
           var castDate = moment(listEl.dt_txt);
           console.log(index);
+          $("#day" + index).text("");
         $("#day" + index).prepend(castDate.format("dddd") + ": " + foreTemp);
         var imgI = $("<img>");
         var iconcode = listEl.weather[0].icon;
